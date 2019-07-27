08c3572c84667dc4874a884ecd7d394d8d6b36b0
fartem <jaman.smlnsk@gmail.com>
2019-07-27 17:40:43 +0300

    2019-07-27 Version 2.9.6: Updated UI tests

---
+++ app/src/androidTest/java/com/smlnskgmail/jaman/hashchecker/ui/screenrunner/ScreenRunnerTest.java
@@ -0,0 +1,86 @@
+package com.smlnskgmail.jaman.hashchecker.ui.screenrunner;
+
+import android.content.Context;
+
+import androidx.annotation.IdRes;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.rule.ActivityTestRule;
+
+import com.smlnskgmail.jaman.hashchecker.MainActivity;
+import com.smlnskgmail.jaman.hashchecker.R;
+import com.smlnskgmail.jaman.hashchecker.ui.BaseUITest;
+
+import org.junit.Rule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@Deprecated
+public class ScreenRunnerTest extends BaseUITest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> menuActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Override
+    public void runTest() {
+        showHashTypesSelector();
+        showResourcesActions();
+        showHashActions();
+
+        showSettingsFragment();
+        showHistoryFragment();
+        showFragmentInMenu(R.string.menu_title_feedback, true);
+    }
+
+    private void showHashTypesSelector() {
+        showBottomSheet(R.id.tv_selected_hash_type);
+    }
+
+    private void showResourcesActions() {
+        showBottomSheet(R.id.btn_generate_from);
+    }
+
+    private void showHashActions() {
+        showBottomSheet(R.id.btn_hash_actions);
+    }
+
+    private void showBottomSheet(int bottomSheetButtonResId) {
+        clickById(bottomSheetButtonResId);
+        delayAndBack();
+    }
+
+    private void showSettingsFragment() {
+        showFragmentInMenu(R.string.menu_title_settings, false);
+        clickToSettingsItem(R.string.settings_title_theme);
+        clickToSettingsItem(R.string.settings_title_author);
+        delayAndBack();
+    }
+
+    private void showFragmentInMenu(@IdRes int menuTitleResId, boolean withBackAction) {
+        Context context = getContext();
+        openActionBarOverflowOrOptionsMenu(context);
+        clickByText(context.getString(menuTitleResId));
+        if (withBackAction) {
+            delayAndBack();
+        }
+    }
+
+    private void showHistoryFragment() {
+        clickById(R.id.menu_main_section_history);
+        delayAndBack();
+    }
+
+    private void clickToSettingsItem(int settingsTitleResId) {
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions
+                        .actionOnItem(hasDescendant(withText(settingsTitleResId)),
+                        click()));
+        delayAndBack();
+    }
+
+}
