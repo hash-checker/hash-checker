925ef830eaa41bb89ef2a1f3c35b2e9fafd4adcf
fartem <jaman.smlnsk@gmail.com>
2021-01-30 10:13:11 +0300

    2021-01-30 Version 3.6.1: Updated build output names and script for screenshots creation

--- make_screenshots.sh
+++ make_screenshots.sh
@@ -3,4 +3,4 @@
 ./gradlew assembleDebug
 ./gradlew assembleDebugAndroidTest
 
-fastlane screengrab --app_package_name=com.smlnskgmail.jaman.hashchecker --tests_apk_path=app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk --app_apk_path=app/build/outputs/apk/debug/hash-checker_3.6.0.apk --output_directory=fastlane/screenshots --test_instrumentation_runner=com.smlnskgmail.jaman.hashchecker.runner.AndroidJacocoTestRunner --use_tests_in_classes=com.smlnskgmail.jaman.hashchecker.screenshots.ScreenshotsTestSuite
+fastlane screengrab --app_package_name=com.smlnskgmail.jaman.hashchecker --tests_apk_path=app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk --app_apk_path=app/build/outputs/apk/debug/hash-checker_debug.apk --output_directory=fastlane/screenshots --test_instrumentation_runner=com.smlnskgmail.jaman.hashchecker.runner.AndroidJacocoTestRunner --use_tests_in_classes=com.smlnskgmail.jaman.hashchecker.screenshots.ScreenshotsTestSuite
