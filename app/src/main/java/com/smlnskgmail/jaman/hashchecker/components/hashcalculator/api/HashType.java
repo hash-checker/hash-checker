a20ef331114661c0c5dd5aac095bc246c909fdfa
fartem <jaman.smlnsk@gmail.com>
2021-12-05 11:50:19 +0300

    2021-12-05 v. 3.6.9: closed #161

--- app/src/main/java/com/smlnskgmail/jaman/hashchecker/components/hashcalculator/api/HashType.java
+++ app/src/main/java/com/smlnskgmail/jaman/hashchecker/components/hashcalculator/api/HashType.java
@@ -10,22 +10,24 @@ import com.smlnskgmail.jaman.hashchecker.ui.bottomsheets.lists.ListItem;
 
 public enum HashType implements ListItem {
 
-    MD5("MD5"),
-    SHA_1("SHA-1"),
-    SHA_224("SHA-224"),
-    SHA_256("SHA-256"),
-    SHA_384("SHA-384"),
-    SHA_512("SHA-512"),
-    SHA3_224(Constants.SHA3_224),
-    SHA3_256(Constants.SHA3_256),
-    SHA3_384(Constants.SHA3_384),
-    SHA3_512(Constants.SHA3_512),
-    CRC_32("CRC-32");
+    MD5("MD5", "md5"),
+    SHA_1("SHA-1", "sha1"),
+    SHA_224("SHA-224", "sha224"),
+    SHA_256("SHA-256", "sha256"),
+    SHA_384("SHA-384", "sha384"),
+    SHA_512("SHA-512", "sha512"),
+    SHA3_224(Constants.SHA3_224, "sha3_224"),
+    SHA3_256(Constants.SHA3_256, "sha3_256"),
+    SHA3_384(Constants.SHA3_384, "sha3_384"),
+    SHA3_512(Constants.SHA3_512, "sha3_512"),
+    CRC_32("CRC-32", "crc32");
 
     private final String hashName;
+    private final String hashFileExtension;
 
-    HashType(String hashName) {
+    HashType(@NonNull String hashName, @NonNull String hashFileExtension) {
         this.hashName = hashName;
+        this.hashFileExtension = hashFileExtension;
     }
 
     @NonNull
@@ -62,6 +64,11 @@ public enum HashType implements ListItem {
     @Override
     public int getAdditionalIconResId() {
         return R.drawable.ic_done;
+    }
+
+    @NonNull
+    public String getFileExtension() {
+        return hashFileExtension;
     }
 
     public boolean isKeccakj() {
