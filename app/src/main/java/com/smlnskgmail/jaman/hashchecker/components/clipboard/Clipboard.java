c31e4bffcc4252964712516226a0aa3bd324a894
Manish Dait <daitmanish88@gmail.com>
2023-06-27 08:17:29 +0530

    2023-06-27 Added: feature to remove the clipboard.

--- app/src/main/java/com/smlnskgmail/jaman/hashchecker/components/clipboard/Clipboard.java
+++ app/src/main/java/com/smlnskgmail/jaman/hashchecker/components/clipboard/Clipboard.java
@@ -3,6 +3,8 @@ package com.smlnskgmail.jaman.hashchecker.components.clipboard;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
@@ -12,17 +14,28 @@ public class Clipboard {
 
     private final Context context;
     private final String text;
+    private final ClipboardManager clipboard;
+    private final Handler handler = new Handler();
+    private final Runnable runnable = this::clear;
 
     public Clipboard(@NonNull Context context, @NonNull String text) {
         this.context = context;
         this.text = text;
+        this.clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
     }
 
     public void copy() {
-        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
         if (clipboard != null) {
             clipboard.setPrimaryClip(ClipData.newPlainText(context.getString(R.string.common_app_name), text));
+            handler.removeCallbacks(runnable);
+            handler.postDelayed(runnable, 60000);
         }
+    }
+
+    private void clear() {
+        ClipData clipData = ClipData.newPlainText(context.getString(R.string.common_app_name), "");
+        Log.i("Clipboard", "Remove Clipboard Text with (Label) : " + context.getString(R.string.common_app_name));
+        clipboard.setPrimaryClip(clipData);
     }
 
 }
