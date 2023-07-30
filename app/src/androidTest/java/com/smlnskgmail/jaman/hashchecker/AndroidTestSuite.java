39c23c12f51682648e761d7551d24cdb569b651d
fartem <jaman.smlnsk@gmail.com>
2023-07-30 10:21:00 +0300

    2023-07-30 v. 3.7.2: updated tests

--- app/src/androidTest/java/com/smlnskgmail/jaman/hashchecker/AndroidTestSuite.java
+++ app/src/androidTest/java/com/smlnskgmail/jaman/hashchecker/AndroidTestSuite.java
@@ -2,8 +2,15 @@ package com.smlnskgmail.jaman.hashchecker;
 
 import com.smlnskgmail.jaman.hashchecker.calculator.GenerateHashFromTextTest;
 import com.smlnskgmail.jaman.hashchecker.calculator.HashCalculatorTaskExceptionTest;
+import com.smlnskgmail.jaman.hashchecker.calculator.jdk.blake2b.Blake2bJdkHashCalculatorTest;
 import com.smlnskgmail.jaman.hashchecker.calculator.jdk.crc.CRC32JdkHashCalculatorTest;
 import com.smlnskgmail.jaman.hashchecker.calculator.jdk.exceptions.JdkHashCalculatorExceptionsTest;
+import com.smlnskgmail.jaman.hashchecker.calculator.jdk.fnv1a.FNV1A1024HashCalculatorTest;
+import com.smlnskgmail.jaman.hashchecker.calculator.jdk.fnv1a.FNV1A128HashCalculatorTest;
+import com.smlnskgmail.jaman.hashchecker.calculator.jdk.fnv1a.FNV1A256HashCalculatorTest;
+import com.smlnskgmail.jaman.hashchecker.calculator.jdk.fnv1a.FNV1A32HashCalculatorTest;
+import com.smlnskgmail.jaman.hashchecker.calculator.jdk.fnv1a.FNV1A512HashCalculatorTest;
+import com.smlnskgmail.jaman.hashchecker.calculator.jdk.fnv1a.FNV1A64HashCalculatorTest;
 import com.smlnskgmail.jaman.hashchecker.calculator.jdk.md.MD5JdkHashCalculatorTest;
 import com.smlnskgmail.jaman.hashchecker.calculator.jdk.sha.SHA1JdkHashCalculatorTest;
 import com.smlnskgmail.jaman.hashchecker.calculator.jdk.sha.SHA224JdkHashCalculatorTest;
@@ -15,6 +22,7 @@ import com.smlnskgmail.jaman.hashchecker.database.LocalDataExporterTest;
 import com.smlnskgmail.jaman.hashchecker.feedback.FeedbackTest;
 import com.smlnskgmail.jaman.hashchecker.rateapp.RateAppDialogTest;
 import com.smlnskgmail.jaman.hashchecker.screenrunner.ScreenRunnerTest;
+import com.smlnskgmail.jaman.hashchecker.settings.HashValueInUpperCaseTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -28,6 +36,13 @@ import org.junit.runners.Suite;
         SHA384JdkHashCalculatorTest.class,
         SHA512JdkHashCalculatorTest.class,
         CRC32JdkHashCalculatorTest.class,
+        Blake2bJdkHashCalculatorTest.class,
+        FNV1A32HashCalculatorTest.class,
+        FNV1A64HashCalculatorTest.class,
+        FNV1A128HashCalculatorTest.class,
+        FNV1A256HashCalculatorTest.class,
+        FNV1A512HashCalculatorTest.class,
+        FNV1A1024HashCalculatorTest.class,
         JdkHashCalculatorExceptionsTest.class,
         MessageDigestZeroLeadsJdkHashCalculatorTest.class,
         GenerateHashFromTextTest.class,
@@ -35,8 +50,7 @@ import org.junit.runners.Suite;
         FeedbackTest.class,
         RateAppDialogTest.class,
         LocalDataExporterTest.class,
-//        HashValueInUpperCaseTest.class,
-//        HashValueInLowerCaseTest.class,
+        HashValueInUpperCaseTest.class,
         ScreenRunnerTest.class,
 })
 public class AndroidTestSuite {
